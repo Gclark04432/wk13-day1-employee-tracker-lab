9a01152ac17d3e70a753b6a7a14ee3a9bcbbe447
Gary Clark <gclark04432@live.co.uk>
2020-02-24 14:33:51 +0000

    add controller to allow path to index and show

--- src/main/java/com/codeclan/example/employeetracker/controllers/EmployeeController.java
+++ src/main/java/com/codeclan/example/employeetracker/controllers/EmployeeController.java
@@ -1,4 +1,31 @@
 package com.codeclan.example.employeetracker.controllers;
 
+import com.codeclan.example.employeetracker.models.Employee;
+import com.codeclan.example.employeetracker.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/employees")
 public class EmployeeController {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Employee> getEmployeeByID(@PathVariable Long id) {
+        return employeeRepository.findById(id);
+    }
+
 }
