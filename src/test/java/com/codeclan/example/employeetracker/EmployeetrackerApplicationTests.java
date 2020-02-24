901a13fe45e0c08b67bb99faf0b368938393967b
Gary Clark <gclark04432@live.co.uk>
2020-02-24 14:29:41 +0000

    add test to ensure can add to database

--- src/test/java/com/codeclan/example/employeetracker/EmployeetrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/employeetracker/EmployeetrackerApplicationTests.java
@@ -1,10 +1,15 @@
 package com.codeclan.example.employeetracker;
 
+import com.codeclan.example.employeetracker.models.Employee;
+import com.codeclan.example.employeetracker.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeetrackerApplicationTests {
+	@Autowired
+	EmployeeRepository employeeRepository;
 
 	@Test
 	void contextLoads() {
@@ -12,7 +17,8 @@ class EmployeetrackerApplicationTests {
 
 	@Test
 	public void canAddEmployee(){
-
+		Employee employee1 = new Employee("Gary", 33, "B1234", "theGreatest@life.com");
+		employeeRepository.save(employee1);
 	}
 
 }
